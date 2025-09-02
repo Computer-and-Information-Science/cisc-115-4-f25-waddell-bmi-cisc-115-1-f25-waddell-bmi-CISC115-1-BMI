beac9e92824658870911215c13bce6e9ac0ff916
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2025-09-02 16:30:23 +0000

    created code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
- 
+  const int height = 61;
+  const int weight = 130;
+
+ //convert to kilograms
+
+ double kg = weight * 0.453592;
+ double meters = height * 0.0254;
+//compute bmi
+  double bmi = kg / (meters*meters);
+
+
+cout << "The BMI for this person is : " << bmi << "."<< endl;
 
 
 }
